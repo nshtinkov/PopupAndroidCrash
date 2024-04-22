976bbf4f72d80ca0d296de696a3c3fb62b25021e
Nikolay Shtinkov <nshtinkov@gmail.com>
2024-04-22 10:28:27 -0400

    Add project files.

---
+++ PopupAndroidCrash/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace PopupAndroidCrash
+{
+	public partial class AppShell : Shell
+	{
+		public AppShell()
+		{
+			InitializeComponent();
+		}
+	}
+}
---
+++ PopupAndroidCrash/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace PopupAndroidCrash
+{
+	[Register("AppDelegate")]
+	public class AppDelegate : MauiUIApplicationDelegate
+	{
+		protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+	}
+}
